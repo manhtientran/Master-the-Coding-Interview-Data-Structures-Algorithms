f613e052a2d30e56dfe01542e4eeb371c25ffb9a
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-20 17:38:20 +0700

    Small exercise for Section 3

---
+++ Section 3. Big O/Twitter.js
@@ -0,0 +1,12 @@
+// Find 1st, Find N-th,...
+
+const array = [
+  { tweet: "hi", date: 2012 },
+  { tweet: "my", date: 2014 },
+  { tweet: "teddy", date: 2018 },
+];
+
+array[0]; // O(1)
+array[array.length - 1]; // O(1)
+
+// O(n^2)
