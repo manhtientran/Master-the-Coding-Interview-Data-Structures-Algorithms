e3c3d916b2c95f8c319cb541ebc88d2890719341
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-20 16:34:28 +0700

    Big O Rule 3

---
+++ Section 3. Big O/Big O Rule 2.js
@@ -0,0 +1,27 @@
+function blah(items) {
+  console.log(items[0]);
+  var middleIndex = Math.floor(items.length / 2);
+  var index = 0;
+
+  while (index < middleIndex) {
+    console.log(items[index]);
+    index++;
+  }
+
+  for (var i = 0; i < 100; i++) {
+    console.log("hi");
+  }
+}
+
+// Log all pairs of array
+const boxes = [1, 2, 3, 4, 5];
+
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes);
