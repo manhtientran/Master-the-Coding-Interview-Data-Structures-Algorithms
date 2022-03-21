49c3ee03128af469c3ca4713af85a2e0ae6d730e
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-21 12:33:54 +0700

    Easy Google coding problem

---
+++ Section 4. How to Solve Coding Problems/GG problem.js
@@ -0,0 +1,38 @@
+function hasPairWithSum(arr, sum) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (mySet.has(arr[i])) {
+      return true;
+    }
+    mySet.add(sum - arr[i]);
+  }
+  return false;
+}
+
+function hasPairWithSum3(arr, sum) {
+  const mySet = new Set();
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let comp = sum - arr[i];
+    if (mySet.has(comp)) {
+      return true;
+    }
+    mySet.add(arr[i]);
+  }
+  return false;
+}
+
+console.log(hasPairWithSum3([6, 4, 3, 2, 1, 7], 9));
